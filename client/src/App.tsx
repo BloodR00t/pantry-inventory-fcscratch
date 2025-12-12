8c496224c72356ce0b79aca1b7e87096c6062302
5Runi <rgeldner@pm.me>
2025-12-12 14:20:06 -0500

    think i did nothig on this branch but watch yesterdays work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import Header from './components/header/Header'
 import CreatePantryItemContainer from './components/create-container/CreatePantryItemContainer'
 import Footer from './components/footer/Footer'
 import PantryItemContainer from './components/pantry/PantryItemContainer'
+<<<<<<< HEAD
+=======
+import styles from 'client/src/components/header/header.css'
+>>>>>>> 75f95e612c8f126a5c29181fc6eb1373624c0a0d
 
 
 
